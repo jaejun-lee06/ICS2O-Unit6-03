33f426b4d0010d2d63b4ac134ef8f7426f539fa1
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-06-22 16:55:35 +0000

    siba

--- js/script.js
+++ js/script.js
@@ -1,35 +1,49 @@
-// Created by: Kenny Le
-// Created on: May 2022
+// Created by: Jaejun Lee
+// Created on: June 2022
 // This file contains the JS functions for index.html
 
-'use strict'
-
 /**
- * Check service worker.
+ * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit6-03/sw.js", {
-    scope: "/ICS2O-Unit6-03/",
-  })
+  navigator.serviceWorker.register("/ICS2O-Unit6-03-JS/sw.js", {
+    scope: "/ICS2O-Unit6-03-JS/",
+  });
 }
 
 /**
- * Get API for weather.
-*/
+ * Get API info.
+ */
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+/**
+ * Get API info.
+ */
 const getWeather = async (URLAddress) => {
   try {
-    const result = await fetch(URLAddress)
-    const jsonData = await result.json()
-    console.log(jsonData.main.temp)
-    const temperature = jsonData.main.temp - 273.15
-    const symbol = jsonData.weather[0].icon
-    const description = jsonData.weather[0].description
-    document.getElementById("api-weather").innerHTML = "The current weather is " + temperature.toFixed(0) + "°C" + "<img src='https://openweathermap.org/img/wn/" + symbol + "@2x.png' alt='Weather Icon' width='15%'>" + "with " + description
+    const request = await fetch(URLAddress);
+    const jsonData = await request.json();
+    var tempKalvin = jsonData.main.temp;
+    var tempCelsius = 0;
+    const feeling = jsonData.weather[0];
+    const image = feeling.icon;
+
+    console.log(jsonData.weather);
+    document.getElementById("weather-image").innerHTML =
+      "<img src='http://openweathermap.org/img/wn/" +
+      image +
+      "@2x.png' alt='Weather Icon' width='10%'><br><h5>";
+    (">");
+
+    // Calculates from Kalvin to Celsius
+    tempCelsius = tempKalvin - 273.15;
+
+    document.getElementById("weather-api").innerHTML =
+      "The current weather is " + tempCelsius.toFixed(2) + " °C";
   } catch (err) {
-    console.log(err)
-    document.getElementById("api-weather").innerHTML = "Error fetching current weather."
+    console.log(err);
   }
-}
-
-
-getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+};
+getWeather(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+);
